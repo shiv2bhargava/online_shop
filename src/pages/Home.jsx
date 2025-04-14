1c934ddc3f6e910a93342a147a18d4ccec3eaecc
Tarun Bhargava <91750721+shiv2bhargava@users.noreply.github.com>
2025-04-14 12:12:03 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 export default function Home() {
     return (
         <Container className="text-center">
-            <h1>Welcome to Store</h1>
+            <h1>Welcome to gaming Store</h1>
             <p>Find the best gaming gear at the best prices!</p>
         </Container>
     );
